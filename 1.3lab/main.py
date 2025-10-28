8802bd25c113d8c29afb73d7e582ecdaf70d2efc
Alex040704 <lesha.kovalenko.00@inbox.ru>
2025-10-28 14:13:57 +0300

    Update main.py

--- 1.3lab/main.py
+++ 1.3lab/main.py
@@ -1,2 +1,8 @@
-if __name__ == "__main__":
-    pass
+import math
+
+n = int(input())
+x = float(input())
+total = 0
+for k in range(0, n + 1):
+    total += ((-1) ** k) * (x ** (2 * k + 1)) / (2 * k + 1)
+print(total)
