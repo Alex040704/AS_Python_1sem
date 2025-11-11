ab5decf563077cbae11fc0a29859cedb459d742c
Alex040704 <lesha.kovalenko.00@inbox.ru>
2025-11-11 14:23:02 +0300

    Update main.py

--- 2.3lab/main.py
+++ 2.3lab/main.py
@@ -1,2 +1,20 @@
-if __name__ == "__main__":
-    pass
+def RemoveCols(A, K1, K2):
+    M = len(A)
+    if M == 0:
+        return A
+    N = len(A[0])
+    
+    if K1 > N:
+        return A
+    
+    end_col = min(K2, N)
+    
+    result = []
+    for i in range(M):
+        new_row = []
+        for j in range(N):
+            if j < K1 - 1 or j >= end_col:
+                new_row.append(A[i][j])
+        result.append(new_row)
+    
+    return result
