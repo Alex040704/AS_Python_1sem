63f3788c6583474ef14fc17e873a35f60f8b5433
Alex040704 <lesha.kovalenko.00@inbox.ru>
2025-11-11 14:14:27 +0300

    Update main.py

--- 1.4lab/main.py
+++ 1.4lab/main.py
@@ -1,2 +1,11 @@
-if __name__ == "__main__":
-    pass
+N = int(input())
+A = int(input())
+B = int(input())
+
+result = [A, B]
+
+for i in range(2, N):
+    next_element = sum(result)
+    result.append(next_element)
+
+print(result)
