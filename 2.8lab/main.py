e7e6337743019d6aa9cd17a5632a71eb3bd261d3
Alex040704 <lesha.kovalenko.00@inbox.ru>
2025-12-02 11:57:34 +0300

    Update main.py

--- 2.8lab/main.py
+++ 2.8lab/main.py
@@ -1,2 +1,12 @@
-if __name__ == "__main__":
-    pass
+with open('f.txt', 'r') as f:
+    numbers = list(map(int, f.read().split()))
+
+positives = [x for x in numbers if x > 0]
+negatives = [x for x in numbers if x < 0]
+
+result = []
+for i in range(0, len(positives), 2):
+    result.extend([positives[i], positives[i+1], negatives[i], negatives[i+1]])
+
+with open('g.txt', 'w') as g:
+    g.write(' '.join(map(str, result)))
